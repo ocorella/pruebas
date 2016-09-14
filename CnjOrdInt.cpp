cc62cb1cc7eb1153ddedf91ab540a398ad182ccc
jpvar <jpvar04@gmail.com>
2016-09-14 10:19:57 -0600

    buscar implementado

--- CnjOrdInt.cpp
+++ CnjOrdInt.cpp
@@ -27,6 +27,20 @@ bool CnjOrdInt::eliminar(int x) {
 }
 
 bool CnjOrdInt::buscar(int x) const {
+	bool rsl = false;
+	NdoInt* p = inicio;
+	while(p != 0){
+		if (p->dato == x){ // ya se encontró x en *this
+			   rsl = true;
+			   p = 0;
+		   } else { // todavía no se encuentra pero puede que esté más adelante
+			   if (p->dato < x)
+				   p = p->sgt;
+			   else // se concluye que x no está en *this
+				   p = 0;
+		   }
+	   }
+	   return rsl;
 
 }
 
